956d6f98787b5704682849446386ab4da0e76dc8
THE ARCHITECT <markusmarwin00@gmail.com>
2024-12-30 20:19:37 +0530

    Update must_join.py

--- SESSIONGEN/must_join.py
+++ SESSIONGEN/must_join.py
@@ -20,11 +20,11 @@ async def must_join_channel(bot: Client, msg: Message):
                 link = chat_info.invite_link
             try:
                 await msg.reply_photo(
-                    photo="https://telegra.ph/file/b790c0caa5324113c3434.jpg", caption=f"» Untuk menggunakan bot ini, anda harus [bergabung]({link}) dulu ke support chat, Jika sudah klik /start kembali!",
+                    photo="https://telegra.ph/file/b790c0caa5324113c3434.jpg", caption=f"» 𝒯𝑜 𝓊𝓈𝑒 𝓉𝒽𝒾𝓈 𝒷𝑜𝓉, 𝓎𝑜𝓊 𝓂𝓊𝓈𝓉 [𝒿𝑜𝒾𝓃]({link}) 𝒻𝒾𝓇𝓈𝓉 𝑔𝑜 𝓉𝑜 𝓈𝓊𝓅𝓅𝑜𝓇𝓉 𝒸𝒽𝒶𝓉, 𝒾𝒻 𝒶𝓁𝓇𝑒𝒶𝒹𝓎 𝒸𝓁𝒾𝒸𝓀𝑒𝒹 /start 𝓇𝑒𝓉𝓊𝓇𝓃!",
                     reply_markup=InlineKeyboardMarkup(
                         [
                             [
-                                InlineKeyboardButton("➕ Gabung Dulu", url=link),
+                                InlineKeyboardButton("𝒥𝑜𝒾𝓃 𝒩𝑜𝓌", url=link),
                             ]
                         ]
                     )
@@ -33,4 +33,4 @@ async def must_join_channel(bot: Client, msg: Message):
             except ChatWriteForbidden:
                 pass
     except ChatAdminRequired:
-        print(f"Promosikan saya ke saya ke saluran must join")
+        print(f"𝒟𝑜𝓃'𝓉 𝓂𝒾𝓈𝓈 𝑜𝓊𝓉—𝒸𝑜𝓂𝑒 𝒿𝑜𝒾𝓃 𝓊𝓈 𝑜𝓃 𝓉𝒽𝑒 𝒸𝒽𝒶𝓃𝓃𝑒𝓁!")
